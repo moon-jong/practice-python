e383239c30131131ce2fe56baacd73dafee3bfb1
moonjong <dksjfl@naver.com>
2019-08-21 18:49:13 +0900

    stack

---
+++ stack.py
@@ -0,0 +1,41 @@
+class Stack:
+    pushedList = list()
+    def push(self, data):
+        self.pushedList.append(data)
+    def pop(self):
+        print(self.pushedList.pop())
+    def printStack(self):
+            for i in range(self.count()):
+                print("-----------")
+                print("     %d     " %(self.pushedList[self.count() - (i + 1)]))
+    def count(self):
+        count = len(self.pushedList)
+        return count
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.push(6)
+stack.push(7)
+stack.push(8)
+stack.push(9)
+
+stack.pushedList
+stack.pop()
+stack.printStack()
+
+
+stack.count()
+
+len(stack.pushedList)
+
+stack.push(8)
+stack.pushedList[2]
+stack.printStack()
+
+for i in range(3):
+    print(i)
+
