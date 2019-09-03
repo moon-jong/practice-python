e9402ca21f51ff1a0cfee06755f2ee5585ce5ea3
moonjong <dksjfl@naver.com>
2019-09-03 21:14:11 +0900

    que is added

--- urllib.py
+++ urllib.py
@@ -22,3 +22,13 @@ req.add_header('Content-Type', 'application/x-www-form-urlencoded')
 f = urlopen(req)
 print(f.info())
 print(f.read(500).decode('utf-8'))
+Request()
+
+from urllib.request import HTTPBasicAuthHandler, build_opener
+auth_handler = HTTPBasicAuthHandler()
+auth_handler.add_password(realm = 'ksh', user = 'shkim', passwd= ' shkimadmin',
+                          uri= 'http://127.0.0.1:8000/auth/',)
+
+opener = build_opener(auth_handler)
+resp = opener.open('http://127.0.0.1:8000/auth/')
+print(resp.read().decode('utf-8'))
